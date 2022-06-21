557de9c3423bbfd37414a03135bf2048275a9cfa
carleoner <kubaw4000@gmail.com>
2022-06-22 00:05:16 +0200

    tuples and dictionaries

---
+++ 10_dictionary.py
@@ -0,0 +1,5 @@
+#example
+#dictionaries are unordered
+
+dic1 = {1914: "WWI", 1939: "WWII", 1999: ["birth", "1 year to 2000"]}
+print(dic1)
---
+++ 9_tuples_immutable_lists.py
@@ -0,0 +1,12 @@
+
+tuple1 = ()
+tuple2 = ("Peter", 4, 3)
+tuple3 = (5, 0, 2)
+
+#tuple2[1] = "EVA" #error
+
+del tuple1
+
+print(tuple2 + tuple3)
+
+# CANNOT use pop() or append() or insert() in tuples
