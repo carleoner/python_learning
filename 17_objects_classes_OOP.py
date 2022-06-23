00a3946189aecad9ff9871935ea4a0bc59f14487
carleoner <kubaw4000@gmail.com>
2022-06-23 14:13:09 +0200

    OOP

--- 17_objects_classes_OOP.py
+++ 17_objects_classes_OOP.py
@@ -30,6 +30,14 @@ class dynamic_emp():
         print("Designation updated")
         return self.empDesignation
 
+class intern (dynamic_emp):
+    def __init__(self, empName, empAge, empDesignation, internPeriod):
+        self.internPeriod = internPeriod
+        dynamic_emp.__init__(self, empName, empAge, empDesignation)
+    
+    def getPeriod(self):
+        return "Intern Period in months: ", self.internPeriod
+
 emp1 = dynamic_emp("Eva", 35, "Dev")
 print("Name of the 1st emp: ", emp1.empName)
 print("Total emps: ", dynamic_emp.totalEmp)
@@ -37,3 +45,19 @@ print("Total emps: ", dynamic_emp.totalEmp)
 print("Details: ", emp1.getEmpDetails())
 emp1.updateDesignation("DevOp")
 print("Details: ", emp1.getEmpDetails())
+
+intern1 = intern("John", 21, "intern", 5)
+print("Intern details: ", intern1.getEmpDetails())
+print(intern1.getPeriod())
+
+#########################################
+# polymorphism
+
+class base:
+    def name():
+        pass
+class derived:
+    def name():
+        print("EOF")
+obj = derived
+obj.name()
